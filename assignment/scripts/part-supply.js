d29e7719caa2599b650d3953bda6575c4bf5720a
John Maras <sportsballenthusiast@gmail.com>
2022-04-15 13:47:39 -0500

    Question six was a real bugger but I think I got it

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -30,13 +30,14 @@ console.log("5. Adding 25 to supplyChanges.", supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log("6. Showing supplyChanges...");
-for(let x = 0; x > supplyChanges.length, x++ ){
-    if(supplyChanges[x] > 0 ){ 
-    console.log('Added x parts.');
-    }
-    else if(supplyChanges[x]=== 0){
-        console.log('No Change.')
-    }
+for (let x = 0; x < supplyChanges.length; x++) {
+  if (supplyChanges[x] > 0) {
+    console.log(`Added ${supplyChanges[x]} parts.`);
+  } else if (supplyChanges[x] === 0) {
+    console.log(`No Change.`);
+  } else if (supplyChanges[x] < 0) {
+    console.log(`Removed ${Math.abs(supplyChanges[x])} parts.`);
+  }
 }
 
 // STRETCH GOALS
